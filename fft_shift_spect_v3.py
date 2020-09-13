1215e2bc002d304a295705d5d4535e5a042a19a2
ochiba11 <64425403+ochiba11@users.noreply.github.com>
2020-09-14 00:24:34 +0900

    Add files via upload
    you can show 2d_spectrum→1d_spectrum

--- fft_shift_spect_v3.py
+++ fft_shift_spect_v3.py
@@ -1,126 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.fftpack as fft
+import scipy.fftpack as sfft
 import cv2
 import os
 from PIL import Image
 
-#画像imgの引数idxの4方枠組みのみの画素値を合計する
-#pixel数で正規化(除算)し戻り値とする
-def getOutPix(img,idx):
-    #中心座標取得
-    h,w = img.shape
-    h_center = h / 2 - 1
-    w_center = w / 2 - 1
-    #sum開始位置を算出
-    y1 = int(h_center - idx)
-    x1 = int(w_center - idx)
-    size = idx * 2 + 2
-    y2 = y1 + size
-    x2 = x1 + size
-    all = img[y1:y2,x1:x2].sum()
-    #引き算する合計算出位置
-    if(idx == 0):
-        diff = 0
-        component = all / (size*size)
-    else:
-        dy1 = int(h_center - (idx-1))
-        dx1 = int(w_center - (idx-1))
-        dsize = idx * 2
-        dy2 = dy1 + dsize
-        dx2 = dx1 + dsize
-        diff = img[dy1:dy2,dx1:dx2].sum()
-        component = (all -diff) / ((size*size) - (dsize*dsize))
-
-    return component
-
-
-
-#path設定
-input_path = "input\\"
-input_name = "noize2.png"
 output_path = "output\\"
+input_path = "input\\"
 
-#画像読み込み
-img = cv2.imread(input_path+input_name,0)
-print(img.shape)
-h,w = img.shape
+#1dspect計算
+def dim1_spect_func(abs_z):
+    print("---------dim1_spect()---------")
+    h,w = abs_z.shape
+    size = min(h,w)
+    center = size // 2
+    len = size - center
+    dim1_spect = np.zeros(len)
 
-#fft設定
-fftsize = max(h,w)
-print(fftsize)
+    for i in range(len):
+        print(" i:",i)
+        if(i==0):
+            dim1_spect[i] = abs_z[center,center]
+        else:
+            sum = 0
+            for j in range(h):
+                for k in range(w):
+                    r = (j-center)*(j-center) + (k-center)*(k-center)
+                    i2 = i * i
+                    next_i2 = ((i+1)*(i+1))
+                    if((i2 <= r) and (r < next_i2)):
+                        sum = sum + abs_z[j,k]
+            dim1_spect[i] = sum
 
-#fft
-z = fft.fftshift(fft.fft2(img,(fftsize,fftsize)))
-output_name = "fft.png"
-#plt.plot(np.abs(z))
-plt.plot(np.abs(z))
-plt.savefig(output_path + output_name)
-'''
-#fftの虚数部を実数化
-z_abs = np.absolute(z)
-#次に対数をとるので1未満は1にする
-z_abs[z_abs < 1] = 1
-#power spectlum算出
-P = np.log10(z_abs)
-'''
-tmp = np.abs(z)
-print("ochi",np.amax(tmp))
-tmp2 = (tmp / np.amax(tmp)) * 255
-P = tmp2.astype(np.uint8)
-img_out = Image.fromarray(P)
-output_name = "spectrum_ochi.png"
-img_out.save(output_path + output_name)
+    nikist = 300
+    lpi = nikist / len
+    plt.figure()
+    plt.style.use('dark_background')
+    plt.xlabel("LineScreenFreq(lpi)")
+    plt.ylabel("Power")
+    plt.xticks([0,75//lpi,150//lpi,225//lpi,300//lpi],[0,75,150,225,300])
+    plt.plot(dim1_spect)
+    output_name = "1dim_spect.png"
+    plt.savefig(output_path+output_name)
+    return dim1_spect
 
-#power spectrum算出
-tmp = np.log(np.abs(z)+1)
-tmp2 = tmp / np.amax(tmp)*255
-#P = 20 * np.log(np.abs(z)+1)
-P = tmp2.astype(np.uint8)
-'''
-#[0,255]に正規化
-P_norm = P / np.amax(P)
-y = np.uint8(np.around(P_norm*255))
-print(y.shape) 
-'''
-y = np.uint8(P)
-#画像出力
-img_out = Image.fromarray(y)
-output_name = "spectrum.png"
-img_out.save(output_path + output_name)
+#FFT
+def fft_func(img):
+    print("---------fft()---------")
+    dc = np.average(img)
+    #dc = 0
+    ac_img = img - dc
+    z = np.fft.fftshift(np.fft.fft2(ac_img))
 
-#------周波数パワーを算出------
-#画素値を円周上に取得し1次元配列にする
-spectrum_size = y.shape
-roop_num = int(spectrum_size[0] / 2)
-normalize_power = np.zeros((roop_num))
-for i in range(roop_num):
-    normalize_power[i] = getOutPix(np.abs(z),i)
+    abs_z = np.abs(z)
+    output_name = "fft.png"
+    plt.plot(abs_z)
+    plt.savefig(output_path + output_name)
 
-print(normalize_power)
-plt.figure()
-plt.plot(normalize_power[50:])
-#plt.show()
-output_name = "freq.png"
-plt.savefig(output_path + output_name)
+    dim2_spect = abs_z
+    dim2_spect[dim2_spect < 1] = 1
+    dim2_spect = 20*np.log(abs_z)
+    img_out = np.uint8(dim2_spect)
+    img_out = Image.fromarray(img_out)
+    output_name = "2dspect.png"
+    img_out.save(output_path + output_name)
+    
+    return abs_z,dc,dim2_spect,z
 
+#逆fft
+def ifft_func(z,dc):
+    print("---------ifft()---------")
+    ac_img = np.fft.ifft2(np.fft.ifftshift(z))
+    img = np.abs(ac_img+dc)
+    img_out = Image.fromarray(np.uint8(img))
+    output_name = "ifft.png"
+    img_out.save(output_path + output_name)
+    return img
 
-#fft.freqによるパワースペクトル算出
-time_step = 1
-freqs = np.fft.fftfreq(h,time_step)
-idx = np.argsort(freqs)
-ps = np.abs(np.fft.fft(img))**2
-plt.figure()
-plt.plot(freqs[idx],ps[idx])
-output_name = "freq_pack.png"
-plt.savefig(output_path+output_name)
+if __name__ == "__main__":
+    #画像読み込み
+    input_name = "noize.png" 
+    img = cv2.imread(input_path+input_name,0)
 
-#fft結果のx応答のみを算出する(=y波0)
-idx = int(h/2 -1)
-tmp = z[idx]
-print(tmp)
-x_spectrum = np.abs(tmp)
-plt.figure()
-plt.plot(x_spectrum[:idx+1])
-output_name = "xspectrum.png"
-plt.savefig(output_path+output_name)
+    #fft
+    abs_z,dc,dim2_spect,z = fft_func(img)
+
+    #1dfft
+    dim1_spect = dim1_spect_func(abs_z)
+
+    #ifft
+    ret_img = ifft_func(z,dc)
